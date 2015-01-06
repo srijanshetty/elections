13cbb4db8379eb0077aa421b8610a13a1cce3cbb
Srijan R Shetty <srijan.shetty@gmail.com>
2015-01-07 00:39:47 +0530

    Batch view is complete, working on senator view

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,44 @@
 var electionsApp = angular.module('electionsApp', ['ui.router']);
 
 electionsApp.config(function($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/form/batch');
 
     $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: '/home.html'
+        .state('form', {
+            url: '/form',
+            templateUrl: 'form.html',
+            controller: 'formController',
+            controllerAs: 'form'
         })
-        .state('about', {
-            url: '/about',
-            templateUrl: '/about.html'
+        .state('form.batch', {
+            url: '/batch',
+            templateUrl: 'form-batch.html',
+            controller: function($scope) {
+                $scope.name = 'batch';
+                $scope.code = 1;
+            }
+        })
+        .state('form.senator', {
+            url: '/senator',
+            templateUrl: 'form-senator.html',
+            controller: function($scope) {
+                $scope.name = 'senator';
+                $scope.code = 2;
+            }
         });
+});
+
+electionsApp.controller('formController', function () {
+    this.formData = {};
+
+    this.batches = [
+        { code: 'y10', fullName: 'UG, Y10'},
+        { code: 'y11', fullName: 'UG, Y11'},
+        { code: 'y12', fullName: 'UG, Y12'},
+        { code: 'y13', fullName: 'UG, Y13'}
+    ];
+
+    this.processForm = function() {
+        window.alert('awesome');
+    };
 });
