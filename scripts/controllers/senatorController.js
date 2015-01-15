764c38569e541185a4e362b03acd4427677a6baf
Srijan R Shetty <srijan.shetty@gmail.com>
2015-01-15 14:01:48 +0530

    Senator validation

--- scripts/controllers/senatorController.js
+++ scripts/controllers/senatorController.js
@@ -3,8 +3,38 @@ angular.module('electionsApp')
         // Make a list of senators available to the view
         $scope.senatorList = dataFactory.getSenators($scope.formData.batch);
 
+        // Skip this step if there are no items in t
+        if ( !$scope.senatorList || $scope.senatorList.length === 0) {
+            $state.go('form.president');
+        }
+
         // Process the submit request
         $scope.processSenatorSubmit = function () {
+            if ($scope.senatorList.length >= 3) {
+                if (!$scope.formData.senatorFirst || !$scope.formData.senatorSecond || !$scope.formData.senatorThird) {
+                    window.alert('Enter three preferences');
+                    return;
+                }
+            } else if ($scope.senatorList.length === 2) {
+                if (!$scope.formData.senatorFirst || !$scope.formData.senatorSecond) {
+                    window.alert('Enter two preferences');
+                    return;
+                }
+            } else if ($scope.senatorList.length === 1) {
+                if (!$scope.formData.senatorFirst) {
+                    window.alert('Enter a preference');
+                    return;
+                }
+            }
+
+            // The choice of candidates must be distinct
+            if (($scope.formData.senatorFirst === $scope.formData.senatorSecond) ||
+                       ($scope.formData.senatorSecond === $scope.formData.senatorThird) ||
+                       ($scope.formData.senatorThird === $scope.formData.senatorFirst)) {
+                window.alert('Choices must be distinct');
+                return;
+            }
+
             // Set the next state
             localStorageService.set('nextState', 'president');
 
