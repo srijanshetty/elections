87e249995249b00822c6b950579330caf2fa5cba
Srijan R Shetty <srijan.shetty@gmail.com>
2015-01-22 14:54:00 +0530

    Shuffle candidate lists

--- scripts/main.js
+++ scripts/main.js
@@ -99,3 +99,19 @@ angular.module('electionsApp')
         onEnter: checkLogin
     });
 });
+
+angular.module('electionsApp')
+    .filter('shuffle', function() {
+        var shuffledArr = [],
+            shuffledLength = 0;
+        return function(arr) {
+            var o = arr.slice(0, arr.length);
+            if (shuffledLength === arr.length) {
+                return shuffledArr;
+            }
+            for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            shuffledArr = o;
+            shuffledLength = o.length;
+            return o;
+        };
+    });
