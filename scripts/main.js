4dba443c8c61a62ba79f2ffa5c378a744fa0f47e
Srijan R Shetty <srijan.shetty@gmail.com>
2015-01-29 10:00:17 +0530

    Changed the system for hall-8 elections

--- scripts/main.js
+++ scripts/main.js
@@ -21,92 +21,105 @@ angular.module('electionsApp')
 
     $urlRouterProvider.otherwise('/login');
 
+    /** These are the necessary states */
     $stateProvider
-    .state('login', {
-        url: '/login',
-        templateUrl: 'partials/login.html',
-        controller: 'loginController'
-    })
-    .state('admin', {
-        url: '/admin',
-        templateUrl: 'partials/admin.html',
-        controller: 'adminController'
-    })
-    .state('form', {
-        url: '',
-        abstract: true,
-        templateUrl: 'partials/form.html',
-        controller: 'formController'
-    })
-    .state('form.batch', {
-        url: '/batch',
-        templateUrl: 'partials/form-batch.html',
-        stateName: 'Info',
-        stateCode: ++stateIndex,
-        controller: 'batchController',
-        onEnter: checkLogin
-    })
-    .state('form.senator', {
-        url: '/senator',
-        templateUrl: 'partials/form-senator.html',
-        stateName: 'senator',
-        stateCode: ++stateIndex,
-        controller: 'senatorController',
-        onEnter: checkLogin
-    })
-    .state('form.president', {
-        url: '/president',
-        templateUrl: 'partials/form-executive.html',
-        controller: 'executiveController',
-        stateCode: ++stateIndex,
-        stateName: 'president',
-        nextState: 'games',
-        onEnter: checkLogin
-    })
-    .state('form.games', {
-        url: '/games',
-        templateUrl: 'partials/form-executive.html',
-        controller: 'executiveController',
-        stateCode: ++stateIndex,
-        stateName: 'games',
-        nextState: 'cultural',
-        onEnter: checkLogin
-    })
-    .state('form.cultural', {
-        url: '/cultural',
-        templateUrl: 'partials/form-executive.html',
-        controller: 'executiveController',
-        stateCode: ++stateIndex,
-        stateName: 'cultural',
-        nextState: 'science',
-        onEnter: checkLogin
-    })
-    .state('form.science', {
-        url: '/science',
-        templateUrl: 'partials/form-executive.html',
-        controller: 'executiveController',
-        stateCode: ++stateIndex,
-        stateName: 'science',
-        nextState: 'films',
-        onEnter: checkLogin
-    })
-    .state('form.films', {
-        url: '/films',
-        templateUrl: 'partials/form-executive.html',
-        controller: 'executiveController',
-        stateCode: ++stateIndex,
-        stateName: 'films',
-        nextState: 'submit',
-        onEnter: checkLogin
-    })
-    .state('form.submit', {
-        url: '/submit',
-        templateUrl: 'partials/form-submit.html',
-        stateCode: ++stateIndex,
-        stateName: 'submit',
-        controller: 'submitController',
-        onEnter: checkLogin
-    });
+        .state('login', {
+            url: '/login',
+            templateUrl: 'partials/login.html',
+            controller: 'loginController'
+        })
+        .state('admin', {
+            url: '/admin',
+            templateUrl: 'partials/admin.html',
+            controller: 'adminController'
+        })
+        .state('form', {
+            url: '',
+            abstract: true,
+            templateUrl: 'partials/form.html',
+            controller: 'formController'
+        })
+        .state('form.batch', {
+            url: '/batch',
+            templateUrl: 'partials/form-batch.html',
+            stateName: 'Info',
+            stateCode: ++stateIndex,
+            controller: 'batchController',
+            onEnter: checkLogin
+        })
+        .state('form.senator', {
+            url: '/senator',
+            templateUrl: 'partials/form-senator.html',
+            stateName: 'senator',
+            stateCode: ++stateIndex,
+            controller: 'senatorController',
+            onEnter: checkLogin
+        });
+
+    /** Configurable states */
+    $stateProvider
+        .state('form.president', {
+            url: '/president',
+            templateUrl: 'partials/form-executive.html',
+            controller: 'executiveController',
+            stateCode: ++stateIndex,
+            stateName: 'president',
+            nextState: 'games',
+            onEnter: checkLogin
+        })
+        .state('form.games', {
+            url: '/games',
+            templateUrl: 'partials/form-executive.html',
+            controller: 'executiveController',
+            stateCode: ++stateIndex,
+            stateName: 'games',
+            nextState: 'mess',
+            onEnter: checkLogin
+        })
+        .state('form.mess', {
+            url: '/mess',
+            templateUrl: 'partials/form-executive.html',
+            controller: 'executiveController',
+            stateCode: ++stateIndex,
+            stateName: 'mess',
+            nextState: 'reading',
+            onEnter: checkLogin
+        })
+        .state('form.reading', {
+            url: '/reading',
+            templateUrl: 'partials/form-executive.html',
+            controller: 'executiveController',
+            stateCode: ++stateIndex,
+            stateName: 'reading',
+            nextState: 'maintenance',
+            onEnter: checkLogin
+        })
+        .state('form.maintenance', {
+            url: '/maintenance',
+            templateUrl: 'partials/form-executive.html',
+            controller: 'executiveController',
+            stateCode: ++stateIndex,
+            stateName: 'maintenance',
+            nextState: 'computer',
+            onEnter: checkLogin
+        })
+        .state('form.computer', {
+            url: '/computer',
+            templateUrl: 'partials/form-executive.html',
+            controller: 'executiveController',
+            stateCode: ++stateIndex,
+            stateName: 'computer',
+            nextState: 'submit',
+            onEnter: checkLogin
+        })
+        .state('form.submit', {
+            url: '/submit',
+            templateUrl: 'partials/form-submit.html',
+            stateCode: ++stateIndex,
+            stateName: 'submit',
+            controller: 'submitController',
+            onEnter: checkLogin
+        });
 });
 
 angular.module('electionsApp')
@@ -118,23 +131,23 @@ angular.module('electionsApp')
 
     // The list of gensecs
     exports.gensecs = [
-        { 'id': 101, 'name': 'Gautam Pratap Singh', 'position': 'president', 'image': 'assets/Gautam.jpg' },
-        { 'id': 102, 'name': 'Pushapjeet Singh Sodhi', 'position': 'president', 'image': 'assets/Pushap.jpg' },
-        { 'id': 103, 'name': 'Ashish Aggarwal', 'position': 'cultural', 'image': 'assets/Ashish.jpg' },
-        { 'id': 104, 'name': 'Chirag Jha', 'position': 'science', 'image': 'assets/Chirag.jpg' },
-        { 'id': 105, 'name': 'Rishi Gupta', 'position': 'science', 'image': 'assets/Rishi.jpg' },
-        { 'id': 106, 'name': 'Prateek Mishra', 'position': 'films', 'image': 'assets/Prateek.jpg' },
-        { 'id': 107, 'name': 'Balendu Shekhar', 'position': 'games', 'image': 'assets/Balendu.jpg' },
-        { 'id': 108, 'name': 'M Surya Prakash', 'position': 'games', 'image': 'assets/Surya.jpg' }
+        { 'id': 101, 'name': 'Jitendra Katiyar', 'position': 'president', 'image': 'assets/Jitendra.jpg' },
+        { 'id': 102, 'name': 'Sateesh Kumar Yadav', 'position': 'computer', 'image': 'assets/Sateesh.jpg'},
+        { 'id': 103, 'name': 'Yaswanth Kumar Penke', 'position': 'games', 'image': 'assets/Yaswanth.jpg'},
+        { 'id': 104, 'name': 'Aditya Desai', 'position': 'maintenance', 'image': 'assets/Aditya.jpg'},
+        { 'id': 105, 'name': 'Amarjit Prakashrao Kene', 'position': 'mess', 'image': 'assets/Amarjit.jpg'},
+        { 'id': 106, 'name': 'Anand Prakash Dwivedi', 'position': 'reading', 'image': 'assets/Anand.jpg'}
+
     ];
 
     // Full names of posts
     exports.fullPostNames = {
-        'president' : 'President Students\' Gymkhana',
-        'cultural': 'GenSec, Cultural Council',
-        'science': 'GenSec, SnT Council',
-        'games': 'GenSec, GnS Council',
-        'films': 'GenSec, FMC Council'
+        'president' : 'President & Convener, HEC',
+        'computer': 'Computer Room Secretary',
+        'games': 'Games & Sports Secretary',
+        'maintenance': 'Maintenance Secretary',
+        'mess': 'Mess Secretary',
+        'reading': 'Reading Room & Circulation Library Secretary'
     };
 
     // Settings for the application
